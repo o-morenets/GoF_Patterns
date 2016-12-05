7190df5851d4c020ff52c7787ea11a065ec1d26c
moreners <alexey.morenets@gmail.com>
2016-12-05 16:43:47 +0200

    + Mediator

---
+++ src/behavioral/mediator/MediatorExample.java
@@ -0,0 +1,82 @@
+package behavioral.mediator;
+
+/**
+ * Created by a-morenets (alexey.morenets@gmail.com) on 05.12.2016.
+ */
+
+abstract class Colleague {
+    protected Mediator mediator;
+
+    public Colleague(Mediator mediator) {
+        this.mediator = mediator;
+    }
+
+    public void send(String message) {
+        mediator.send(message, this);
+    }
+
+    public abstract void notify(String message);
+}
+
+abstract class Mediator {
+    public abstract void send(String message, Colleague colleague);
+}
+
+class ConcreteColleague1 extends Colleague {
+    public ConcreteColleague1(Mediator mediator) {
+        super(mediator);
+    }
+
+    @Override
+    public void notify(String message) {
+        System.out.println("Colleague1 gets message: " + message);
+    }
+}
+
+class ConcreteColleague2 extends Colleague {
+    public ConcreteColleague2(Mediator mediator) {
+        super(mediator);
+    }
+
+    @Override
+    public void notify(String message) {
+        System.out.println("Colleague2 gets message: " + message);
+    }
+}
+
+class ConcreteMediator extends Mediator {
+    private ConcreteColleague1 colleague1;
+    private ConcreteColleague2 colleague2;
+
+    public void setColleague1(ConcreteColleague1 colleague) {
+        this.colleague1 = colleague;
+    }
+
+    public void setColleague2(ConcreteColleague2 colleague) {
+        this.colleague2 = colleague;
+    }
+
+    @Override
+    public void send(String message, Colleague colleague) {
+        if (colleague.equals(colleague2)) {
+            colleague1.notify(message);
+        } else {
+            colleague2.notify(message);
+        }
+    }
+}
+
+public class MediatorExample {
+    public static void main(String[] args) {
+        ConcreteMediator m = new ConcreteMediator();
+
+        ConcreteColleague1 c1 = new ConcreteColleague1(m);
+        ConcreteColleague2 c2 = new ConcreteColleague2(m);
+
+        m.setColleague1(c1);
+        m.setColleague2(c2);
+
+        c1.send("How are you?");
+        c2.send("Fine, thanks");
+    }
+}
